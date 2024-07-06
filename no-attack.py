ea010a200980991cb4400f0ab415810d3968128c
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-06 04:29:12 +0200

    Update no-attack.py

--- no-attack.py
+++ no-attack.py
@@ -9,7 +9,7 @@ parser.add_argument("-batch", help="Batch size", nargs='?', type=int, default=12
 parser.add_argument("-lr", help="Learning rate", nargs='?', type=float, default=1e-2)
 parser.add_argument("-momentum", help="Batch size", nargs='?', type=float, default=0.9)
 parser.add_argument("-nparty", help="Number of clients", nargs='?', type=int, default=2)
-parser.add_argument("-backbone", help="Backbone", nargs='?', type=int, default=2)
+parser.add_argument("-backbone", help="Backbone", nargs='?', type=str, default="resnet18")
 args = parser.parse_args()
 
 seedBasic()
