7352af75f9a9c51f88094fd3378441f357a4fbe6
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-06 17:51:37 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -4,13 +4,10 @@ import tensorflow as tf
 def model_factory(backbone: str = "resnet18", n_classes: int = 10, n_attackers: int = 1, n_party: int = 2):
     inputs = []
     features = []
-    backbones = []
     attackerClassifiers = []
     for i in range(n_party):
-        inputImage = tf.keras.layers.Input(shape = (None, None, 3), dtype=tf.uint8, name = f'image_{i}')
-        image = tf.keras.layers.Lambda(lambda data: tf.keras.applications.imagenet_utils.preprocess_input(tf.cast(data, tf.float32), mode="tf"))(inputImage)
-        backbones.append(keras_cv.models.ResNetBackbone.from_preset(backbone, name = f"feature_{i}"))
-        feature = tf.keras.layers.GlobalAveragePooling2D()(backbones[-1](image))
+        inputImage = tf.keras.layers.Input(shape = (None, None, 3), dtype=tf.float32, name = f'image_{i}')
+        feature = tf.keras.layers.GlobalAveragePooling2D()(keras_cv.models.ResNetBackbone.from_preset(backbone, name = f"feature_{i}")(inputImage))
 
         inputs.append(inputImage)
         features.append(feature)
@@ -26,17 +23,6 @@ def model_factory(backbone: str = "resnet18", n_classes: int = 10, n_attackers: 
     model = tf.keras.models.Model(inputs = inputs, outputs = [output])
 
     for i in range(n_attackers):
-        image = tf.keras.layers.Lambda(lambda data: tf.keras.applications.imagenet_utils.preprocess_input(tf.cast(data, tf.float32), mode="tf"))(inputs[i])
-        feature = tf.keras.layers.GlobalAveragePooling2D()(tf.keras.models.clone_model(backbones[i])(image))
-
-        fcnn4 = tf.keras.Sequential([
-            tf.keras.layers.Dense(1024, activation='relu'),
-            tf.keras.layers.Dense(512, activation='relu'),
-            tf.keras.layers.Dense(256, activation='relu'),
-            tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(n_classes, activation='softmax')], name = "output")
-        output = fcnn4(tf.keras.layers.Concatenate()(features))
-        
-        attackerModel = tf.keras.models.Model(inputs = [inputs[i]], outputs = [output])
-        attackerClassifiers.append(attackerModel)
+        attackerModel = tf.keras.models.Model(inputs = [inputs[i]], outputs = [tf.keras.layers.Dense(n_classes, activation='softmax', name="output")(features[i])])
+        attackerClassifiers.append(tf.keras.models.clone_model(attackerModel))
     return model, attackerClassifiers
