a073bb98196afd20eeab4dca8bc32b886212604a
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-08 05:34:37 +0200

    Add step 4

--- attack.py
+++ attack.py
@@ -12,6 +12,7 @@ parser.add_argument("-backbone", help="Backbone", nargs='?', type=str, default="
 
 parser.add_argument("-windowSize", help="Trigger size", nargs='?', type=int, default=3)
 parser.add_argument("-nparty", help="Number of clients", nargs='?', type=int, default=2)
+parser.add_argument("-p", help="Percentage subset of the source class", nargs='?', type=float, default=0.5)
 
 args = parser.parse_args()
 
@@ -39,19 +40,16 @@ with strategy.scope():
                                        loss = {'output': tf.keras.losses.CategoricalCrossentropy()},
                                        metrics = {"output": [tf.keras.metrics.CategoricalAccuracy()]})
 
-maskPositions = {}
+positions = {}
 for i in range(len(attackerClassifiers)):
-    attackerClassifiers[i].fit(auxil_dataset, validation_data = train_dataset, verbose = 1, epochs = 1)
+    attackerClassifiers[i].fit(auxil_dataset, validation_data = train_dataset, verbose = 1, epochs = args.epochs)
     saliency_maps = compute_saliency_map(train_dataset, attackerClassifiers[i])
-    maskPositions[i] = [getMaxWindow(saliency_map, args.windowSize)[0] for saliency_map in tqdm(saliency_maps)]
+    positions[i] = [getMaxWindow(saliency_map, args.windowSize)[0] for saliency_map in tqdm(saliency_maps)]
 
-    x_sub_s_idx = np.where(np.argmax(Y_train, axis=1) == 0)[0][:1000]
-    x_t_idx = np.where(np.argmax(Y_train, axis=1) == 1)[0]
+attackDataGeneration = AttackDataGeneration(model, args.p, X_train, Y_train, positions, 0, 1, args.windowSize, 
+                                            args.batch, strategy, args.lr, args.momentum, args.epochs, n_party=args.nparty)
 
-    x_sub_s = X_train[x_sub_s_idx]
-    positions = np.array([[0, 0] for i in range(len(x_sub_s_idx))])
-    x_t = X_train[x_t_idx]
-
-    triggerData = FindTriggerDataGeneration(x_sub_s, positions, x_t, args.windowSize, args.batch, i, args.nparty)
-    with strategy.scope():
-        triggerModel = buildTriggerModel(model, args.windowSize, i)
+H = model.fit(train_dataset,
+              validation_data = valid_dataset,
+              verbose = 1,
+              epochs = args.epochs)
--- data/DataGeneration.py
+++ data/DataGeneration.py
@@ -2,6 +2,7 @@ import cv2
 import random
 import numpy as np
 import tensorflow as tf
+from model.model import *
 
 class BaseDataGeneration(tf.keras.utils.Sequence):
     def __init__(self, images, labels, batchsize, shuffle = False, n_party = 2, **kwargs):
@@ -34,7 +35,7 @@ class BaseDataGeneration(tf.keras.utils.Sequence):
             else:
                 X[f"image_{i}"] = images[:, :, i*round(images.shape[2]/self.n_party):]
         return X, {"output": labels}
-    
+
 
 class FindTriggerDataGeneration(tf.keras.utils.Sequence):
     def __init__(self, x_sub_s, positions, x_t, windowSize, batchsize, party, n_party, **kwargs):
@@ -68,3 +69,77 @@ class FindTriggerDataGeneration(tf.keras.utils.Sequence):
         x_t = self.x_t[idx][:, :, self.party*round(self.x_sub_s.shape[2]/self.n_party):(self.party+1)*round(self.x_sub_s.shape[2]/self.n_party)]
         masks = self.position_to_masks(self.positions[idx], x_sub_s.shape[1:3])
         return {"x_sub_s": x_sub_s, "x_t": x_t, "masks": masks}, {"output": np.array([0.0]*len(idx))}
+    
+def findTrigger(model, p, images, labels, positions, targetClass, sourceClass, windowSize, batch, nparty, partyIdx, strategy, lr, momentum, epochs):
+    x_sub_s_idx = np.where(np.argmax(labels, axis=1) == sourceClass)[0]
+    x_sub_s_idx = random.choices(x_sub_s_idx, k = round(p*len(x_sub_s_idx)))
+    x_t_idx = np.where(np.argmax(labels, axis=1) == targetClass)[0]
+    x_sub_s = images[x_sub_s_idx]
+    x_t = x_t_idx[x_t_idx]
+
+    triggerData = FindTriggerDataGeneration(x_sub_s, positions, x_t, windowSize, batch, partyIdx, nparty)
+    with strategy.scope():
+        triggerModel = buildTriggerModel(model, windowSize, partyIdx)
+        triggerModel.compile(optimizer = tf.keras.optimizers.SGD(learning_rate=lr, momentum=momentum),
+                    loss = {'output': tf.keras.losses.MeanSquaredError()},
+                    metrics = {"output": [tf.keras.metrics.MeanAbsoluteError()]})
+        
+    triggerModel.fit(triggerData, epochs=epochs, verbose = False)
+    return triggerModel.layers[2].W.numpy()
+
+class AttackDataGeneration(tf.keras.utils.Sequence):
+    def __init__(self, model, p, images, labels, positionsDict, targetClass, sourceClass, windowSize, batchsize, strategy, lr, momentum, epochs, shuffle = False, n_party = 2, **kwargs):
+        self.model = model
+        self.p = p
+        self.ids = list(range(images.shape[0]))
+        self.images = images
+        self.labels = labels
+        self.batchsize = batchsize
+        
+        self.shuffle = shuffle
+        self.n_party = n_party
+
+        self.positionsDict = positionsDict
+        self.targetClass = targetClass
+        self.sourceClass = sourceClass
+        self.windowSize = windowSize
+
+        self.strategy = strategy
+        self.lr = lr
+        self.momentum = momentum
+        self.epochs = epochs
+
+        self.triggers = {k: findTrigger(self.model, self.p, self.images, self.labels, self.positionsDict[k], 
+                                     self.targetClass, self.sourceClass, self.windowSize, self.batchsize, 
+                                     self.n_party, k, self.strategy, self.lr, self.momentum, self.epochs) for k in self.positionsDict.keys()}
+
+        super().__init__(**kwargs)
+    
+    def __len__(self):
+        return len(self.images) // self.batchsize + int(len(self.images) % self.batchsize != 0)
+    
+    def on_epoch_end(self):
+        if self.shuffle:
+            self.ids = sorted(self.ids, key=lambda k: random.random())
+
+        self.triggers = [findTrigger(self.model, self.p, self.images, self.labels, self.positionsDict[k], 
+                                     self.targetClass, self.sourceClass, self.windowSize, self.batchsize, 
+                                     self.n_party, k, self.strategy, self.lr, self.momentum) for k in self.positionsDict.keys()]
+        
+    def __getitem__(self, index):
+        idx = self.ids[index*self.batchsize: min((index+1)*self.batchsize, len(self.ids))]
+        images = self.images[idx]
+        labels = self.labels[idx]
+
+        X = {}
+        for i in range(self.n_party):
+            if i != self.n_party-1:
+                X[f"image_{i}"] = images[:, :, i*round(images.shape[2]/self.n_party):(i+1)*round(images.shape[2]/self.n_party)]
+            else:
+                X[f"image_{i}"] = images[:, :, i*round(images.shape[2]/self.n_party):]
+
+            if i in self.positionsDict:
+                for j in range(len(idx)):
+                    position = self.positionsDict[i][idx[j]]
+                    X[f"image_{i}"][j][position[0]:position[0]+self.windowSize, position[1]:position[1]+self.windowSize] = self.triggers[i]
+        return X, {"output": labels}
